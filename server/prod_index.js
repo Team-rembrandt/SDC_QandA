3359d645abe56ab4109dfefb7affa3f6f0390ec4
Jinhoo Bong <jinhoobong@gmail.com>
2021-08-18 23:54:59 -0700

    added production index and respective npm command

---
+++ server/prod_index.js
@@ -0,0 +1,31 @@
+require('newrelic');
+// setting up dependencies
+const express = require('express');
+let db = require('./db').db;
+let router = require('./routes.js');
+let compression = require('compression');
+let bodyParser = require('body-parser');
+const morgan = require('morgan');
+const request = require('supertest');
+
+const app = express();
+
+module.exports.app = app;
+
+app.use(compression());
+// morgan can show us how long it takes to retrieve data from db
+app.use(morgan('dev'));
+// set port nuber
+app.set('port', 80);
+
+// tells our application to accept incoming JSON body in requests
+// (REST APIs communicate in JSON form)
+app.use(express.json());
+
+// set up our routes
+app.use('/qa', router);
+
+if(!module.parent) {
+  app.listen(app.get('port'));
+  console.log('Listening on', app.get('port'));
+}
