c0908d6803f3349706dc752133f6f7a5e0de134b
Jinhoo Bong <jinhoobong@gmail.com>
2021-08-19 17:04:14 -0700

    removed console log for ppid

--- server/controller/qa.js
+++ server/controller/qa.js
@@ -8,7 +8,6 @@ module.exports = {
   // count
   getQuestions: function(req, res) {
     let productId = req.params.product_id;
-    console.log('pid', productId);
     let page = 1 || req.body.page;
     let counter = 5 || req.body.counter;
     models.qa.getQuestions(productId, page, counter, (err, results) => {
